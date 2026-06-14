ddc37386e0b60ba5566a008285c01f71217171c4
Oppie1 <atindall729@gmail.com>
2026-06-14 13:47:14 -0500

    Add files via upload

---
+++ 1.6CConvrsi0nCharWNotes.EVNLsNts/1.6CConvrsi0nCharWNotes.EVNLsNts/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+
+int main() {
+
+	//Display a message to the screen about how many slices of pie you ate last night using format specifier and
+	//an integer.
+	//CODE:
+
+	//Print a message to the screen that uses 2 string data specifiers and a sentence to say how good Kanopi is at 
+	//being a C++ programmer.
+	//CODE:
+
+	//Demonstrate using %f and %.f to show how to show no decimal places of pi (to 6 digits max), 2 or 4 decimal places.
+	//CODE:
+
+	//CODE:
+
+	//CODE:
+
+}
---
+++ 1.7CVariables.EVNLsNts/1.7CVariables.EVNLsNts/main.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+
+
+/*Variables are named storage locations in memory that hold values. Think of them like labeled
+boxes where you can store and retrieve data, similar to how you use variables in algebra*/
+
+
+int main()
+{
+
+	//Declare 3 integer variables age, currentYear, birthYear. and initialize them with our information.
+	//CODE:
+
+
+	//Write an expression that assigns the age equal to the result of the current year minus the birth year.
+	//CODE:
+
+	//Print message to screen that say Kanopi is... years old and use the age variable and data type specifier to do so.
+	//CODE:
+
+}
