5957a3bb39a17548eb2f54b2876f23d01fecdac2
Oppie1 <atindall729@gmail.com>
2026-06-14 19:36:53 -0500

    Add files via upload

---
+++ 1.11CGettingInputWithScanf.EVNLNts/2.11CGettingInputWithScanf.EVNLNts/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+
+int main() {
+
+	
+	//Declare a character array with 20 uninitialized element slots for first name.
+	//CODE:
+
+	//Declare a character array that holds 20 uninitialized elements to hold your crushes name.
+	//CODE:
+
+	//Declare an uninitialized integer variable that will hold the number of puppies.
+	//CODE:
+
+	//Prompt the user by printing message out to screen requesting their name.
+	//CODE:
+	printf("Hello, what is your name?\n");
+
+	//Scan in user input with parameters for the string ("  "), firstName variable and number of elements.
+	//For arrays, the array name already represents an address, so & is omitted here.
+	//CODE:
+
+	//Prompt user with a print statement and ask them who their crush is.
+	//CODE:
+	printf("Who is your crush?\n");
+
+	//Scan in the users input in the same way you did first name.
+	//CODE:
+
+	//Prompt user with a third print statement asking how many puppies they will have.
+	//CODE:
+	printf("How many puppies will you have together?\n");
+
+	//Must use & for variables memory location since not an array.
+	//Scan in user data with parameter for an int (%d) and the numberOfPuppies variable.
+	//CODE:
+
+	//Call printf() on all three stored values into on readable sentence.
+	//CODE:
+}
---
+++ 1.14CZCalculatingAvgAge.EVNLNts/1.14CZCalculatingAvgAge.EVNLNts/main.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+
+	//Declare three uninitialized integer variables to hold whole number values.
+	//CODE:
+
+	//Assign the same value (say 100) to those variables in a single statement by chaining the assignment
+	//operator (=) from left to right.
+	//CODE:
+
+	//Print the values of a, b and c to the console. 
+	//CODE:
+
+	//Declare three float variables for 3 ages and 1 to store the result of average.
+	//CODE:
+
+	//Assign age1 and age2 equal to each other and the same value "10".
+	//CODE:
+
+	//Call a print function to prompt user to enter an age for age3.
+	//CODE:
+	printf("\nPlease enter your age:\n");
+
+	//Call a scanf_s() function to read in age3 (remember to use &).
+	//CODE:
+
+//Calculate the average by summing all three ages and dividing by 3.
+//Also, you will need to use "()" (age# + age )  because we divide by 3 -> order of operations.
+//CODE:
+
+//Call printf() to show a sentence with average out to the screen.
+//CODE:
+
+}
