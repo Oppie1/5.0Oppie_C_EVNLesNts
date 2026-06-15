72b73a2dc0ca4750991fb1046f75e90445bde20f
Oppie1 <atindall729@gmail.com>
2026-06-15 17:56:13 -0500

    Add files via upload

---
+++ 1.17CDecisnsUsinifStatmnts.EVNLNts/1.17CDecisnsUsinifStatmnts.EVNLNts/main.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>	
+
+
+int main() {
+
+	//Declare an uninitialized int variable called "age" to hold the user's input.
+	//CODE:
+
+	//Call a print() to a prompt the user for their age. 
+	//CODE:
+
+	//Read the int the user types and store it in the variable "age" which passes the 
+	//memory address of "age" to scanf_s, telling it exactly where in memory to write the value.
+	//CODE:
+
+	//Use an if statement to validate whether the person is greater than or equal to 18.
+	//If they are call a print() to say they may enter the website to the screen.
+	//CODE:
+
+	//Create an if statement that does this along with a corresponding sentence 
+	//in the if block that displays to screen.
+	//CODE:
+
+}
---
+++ 1.18CNstinIfStatmnts.EVNLNts/1.18CNstinIfStatmnts.EVNLNts/main.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+int main()
+{
+	//Declare an uninitialized int variable to store the user's age.
+	//CODE:
+
+	//Declare an uninitialized character variable to store the user's gender.
+	//CODE:
+
+	//Prompt the user to enter their age.
+	//CODE:
+	printf("Please enter you age.\n");
+
+	//Call scanf_s() to store age value in int age.
+	//CODE:
+
+	//Prompt user for their gender using a call printf function.
+	//CODE:
+	printf("What is your gender?\n");
+
+	//Call scanf_s() to scan in user data that has 3 parameters. '1' makes sure user ONLY 
+	//enters 1 character 
+	//CODE:
+
+	//Create an outer if statement with the condition if the persons age is greater
+	//than or equal to 18 to print the statement you may enter this website to the screen.
+	//CODE:
+
+		//Use nested if statement check if the user is male 'm' by setting the gender
+		//variable == to m in the if condition. Then print() if male.
+		//CODE:
+		
+		//Now use an if condition in the same fashion to check if the user is a female.
+		//If they are write a sentence that a female has entered the website.
+		//CODE:
+			
+		//Write if condition to test if the person is less than 18. If they
+		//are print a statement that they cannot enter the website to the screen.
+		//CODE:
+
+}
