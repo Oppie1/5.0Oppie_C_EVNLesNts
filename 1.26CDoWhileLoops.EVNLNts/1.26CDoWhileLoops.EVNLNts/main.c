73d215f4a2cafcb323a189ef1baa44fe61b460dc
Oppie1 <atindall729@gmail.com>
2026-06-15 18:03:16 -0500

    Add files via upload

---
+++ 1.26CDoWhileLoops.EVNLNts/1.26CDoWhileLoops.EVNLNts/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+
+int main() {
+
+	//Declare 4 float variables initialized to 0 -> grade, scoreEntered, numberOfTests and average.
+	//CODE:
+
+	//Call a print function to let the user know that they should press 0 when they are done entering grades.
+	//CODE:
+	printf("Press 0 to stop the program\n");
+
+	//do-while loop: executes the block first, then evaluates the condition.
+	//Create a do condition.
+	//CODE:
+		//Call a printf() to show the number of tests and the average to the screen along with helpful sentence (%f).
+		//CODE:	
+
+		//Prompt the user to enter a test score
+		printf("Enter a test score:\n");
+
+		//Call a function to read in the users input and store it in scoreEntered.
+		//CODE:
+
+		//Assign grade to adding the score entered and setting it equal to 
+		//the current score entered.
+		//CODE:
+
+		//Write an expression that increments the number of test by one each iteration
+		//CODE:	
+
+		//Assign the average a value that accounts for the total of the grades entered
+		//divided by the number of test that were input by user.
+		//CODE:
+
+		//Make a while loop condition that runs so long as the score entered does NOT equal 0. Because if 
+		//the user entered 0 the condition would fail and the program would stop here.
+		//CODE:
+
+}
---
+++ 1.27CForLoop.EVNLNts/1.27CForLoop.EVNLNts/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main() {
+
+	//Declare two uninitialized int variables named peanuts and soybeans.
+	//CODE:
+
+	//Use a for loop to initialize peanut to 0. Have loop run so long as peanut is less than 10, increment by 2 each iteration.
+	//CODE:
+
+		//Show a sentence that says how many peanuts their are during each iteration.
+		//CODE:
+
+	printf("\nAnd now for the second example\n\n");
+
+	//Create a for loop where soybean int value starts at 1. For the 
+	//control condition use soybean <= 10 again but this time just increment by 1.
+	//CODE:
+
+		//Now same as above peanut example print out how many soybeans you have for all 10 iterations.
+			//CODE:
+
+}
