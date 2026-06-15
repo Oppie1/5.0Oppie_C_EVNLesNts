e6d9cf6b1db17e3e9c9f148fd9f31b29b6de7bc1
Oppie1 <atindall729@gmail.com>
2026-06-15 18:02:44 -0500

    Add files via upload

---
+++ 1.23CShorthandIfElse.EVNLNts/1.23CShorthandIfElse.EVNLNts/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+
+/*
+Write a program that provides 20 characters buffer space for last name. Ask a persons
+//last name. less than 'M' () () ()
+*/
+
+int main() {
+
+
+	//Declare an uninitialized character array that has 20 elements
+	//CODE:
+
+	//Prompt user to enter their last name.
+	//CODE:
+
+	//Call a scanf_s() function with 3 parameters to see in the users last name. sizeof() is needed.
+	//CODE:
+
+	//Use the ternary operator to determine if the first letter of the persons lastName first0 element 
+	//starts with (Use <M) ? (for if no if in front) "Blue Team" to the screen (else :)  printf("Red Team") screen.
+	//REMEMBER-> (stuff <M) printf(color) () 3
+	//CODE:
+}
