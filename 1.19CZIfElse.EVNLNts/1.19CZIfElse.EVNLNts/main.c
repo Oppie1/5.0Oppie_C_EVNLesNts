a9788bce1d383c630a1d77d5c53b9c2ba7a5af61
Oppie1 <atindall729@gmail.com>
2026-06-15 18:00:45 -0500

    Add files via upload

---
+++ 1.19CZIfElse.EVNLNts/1.19CZIfElse.EVNLNts/main.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+int main() {
+
+	//Declare an uninitialized int variable to hold the user's age.
+	//CODE:
+
+	//Declare a character variable to hold the user's gender. 
+	//CODE:
+
+	//call printf() function to prompt user to enter their age.
+	//CODE:
+	printf("How old are you?\n");
+
+	//call scanf_s() function to scan in the users age. Use the & operator to pass in the age into memory location.
+	//CODE:
+
+	//Call printf() to request the users age.
+	//CODE:
+	printf("\nWhat is your gender m/f?\n");
+
+	//Read in a single character from the console and stores it in memory address of 'gender'.
+	//CODE:
+
+	//Use if condition to test if user is below 18. If they are
+	//print out that they may enter the website.
+	//CODE:
+
+		//Use nested if/else below to determine whether person is a male or female.
+		//CODE:
+
+		//Make a nested else to the first if that executes and says a female has entered the website.
+		//CODE:
+
+	//Make an else condition that prints that the person is not old enough to enter the website.
+	//CODE:
+
+}
