5ed40a818de4f89d7c931fad3a55bd8e57959759
Oppie1 <atindall729@gmail.com>
2026-06-15 18:02:20 -0500

    Add files via upload

---
+++ 1.24CZIncrmntOpratr.EVNLNts/1.24CZIncrmntOpratr.EVNLNts/main.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+	//Declare 3 int variables.  a, b, answer
+	//be initialized to 5 and 10 respectively. answer should be initialized to 0.
+	//CODE:
+	
+	//Assign answer to the expression incrementing a before it is multiplied by b.
+	//CODE:
+
+	//Call printf() with a and 'answer' with a sentence to print a and answer to the screen. 6 and 60
+	//CODE:
+
+	//Declare the variables to the original values since answer and a are currently holding the last expressions result. 
+	//CODE:
+
+	//Assign answer set to increment a after and then multiply by b.
+	//CODE:
+
+	//Call printf() with a and answer with sentence to the screen to show difference with pre increment. 6 and 50
+	//CODE:
+
+	printf("\n------Next Demonstration-------\n\n");
+
+	//Declare an initialized variable named tuna and set it to 20
+	//CODE:
+
+	//Display the initial value of to the screen
+	//CODE:
+
+	//Use pre-increment operator so tuna goes from 20 to 21.
+	//CODE:
+
+	//Now show the new value to the screen.
+	//CODE:
+
+	//Use the pre-increment again to show that tuna keeps that new value and then
+	//adds to it 1 when the increment operator is used again. Then printf() to screen.
+	//CODE:
+
+	//Do the subtraction increment operator -- just to show in example proficiency. And print to screen. 
+	//CODE:
+
+	//Multiply by 2 to see what result you get and then print to screen.
+	//ASSIGN tuna and then tuna to the pre-increment of tuna * 2.
+	//CODE:
+
+	//ASSIGN tuna and then tuna to the post-increment operator to see what happens.
+	//Multiply by 2 like above.
+	//CODE:
+
+}
