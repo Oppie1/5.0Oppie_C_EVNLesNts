b8dee6847c998743f2328059a8e6d3e687085c45
Oppie1 <atindall729@gmail.com>
2026-06-15 18:05:43 -0500

    Add files via upload

---
+++ 1.22Cor.EVNLNts/1.22Cor.EVNLNts/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+
+
+int main() {
+
+	//call a printf() function that says what the program does.
+	//CODE:
+	printf("---This program demonstrates the '|| -> or operator---\n");
+
+	//Declare an uninitialized char variable called 'answer' to store a single character
+	//from the user. 
+	//CODE:
+
+	//Prompt the user a y/n type question like do they like bagels or something.
+	//CODE:
+	printf("Do you like bagels y/n?\n");
+
+	//Scan a single character from the user and store it in 'answer'.
+	//CODE:
+
+	//Use an if condition to test fashion as you did the last tutorial. If true call printf() -> entered y or n.
+	//CODE:
+
+		//Use nested if to test if the user entered a y and if so print that they like bagels.
+		//CODE:
+
+		//Use else statement to signal the user does not like bagels to the screen.
+		//CODE:
+
+
+	printf("\n\n-----------------------------\n");
+	//Just instructions. Do not delete.
+	printf("\n---Second part of program using integers, less than and greater than.---");
+	printf("\nWrite backend program with no user input measuring two sets of if conditions\n");
+	printf("\n");
+
+	//Use an if statement with two conditions that are joined by the OR || operator.  Use this
+	//logic in your example: 5 is not greater than 90 and for the second condition 10 equals == 10. if statement  
+	//is true and you execute the print codeblock which could say 5 may not be greater than 90 but 10 DOES equal 10. 
+	//CODE:
+
+
+	//Now use an else statement that would catch all/confirm that both statements are false
+		//and prints a message declaring that to the screen.
+		//CODE:
+
+}
