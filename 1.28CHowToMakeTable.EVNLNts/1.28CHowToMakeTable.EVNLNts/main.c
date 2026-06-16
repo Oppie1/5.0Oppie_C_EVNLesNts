6975caa414b974303c1a2bf2b639275e131c9099
Oppie1 <atindall729@gmail.com>
2026-06-15 20:07:43 -0500

    Add files via upload

---
+++ 1.28CHowToMakeTable.EVNLNts/1.28CHowToMakeTable.EVNLNts/main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+
+
+//This program demonstrates how to print a simple 3X3 table using nested for loops
+
+int main()
+
+{
+
+	//Declare two uninitialized variables one for row and one for column.
+	//CODE:
+
+	//Write/define an outer for loop that initializes the row to 1, then test if row is less than 4. 
+	//iterate row by 1. 
+	//CODE:
+
+	//Write for loop to set column to 1, have the condition column < 4 and the increment by 1.
+	//CODE:
+
+	//Call a print() to show the current value (column variable) to the screen for each iteration.
+	//CODE:
+
+	//Print a new line here so the next row starts out on a new line.
+	//CODE:
+
+}
---
+++ 1.29.CBreak.EVNts/1.29.CBreak.EVNts/main.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main() {
+
+	//Declare 3 int variables the first two uninitialized.  i a counter variable for for loop, 
+	//howMany -> which will determine if loop should stop before control section
+	//Lastly maxAmount which will will be initialized to 10 - iterations program can have. 
+	//Code:
+
+	//Prompt user asking how many times do they want loop to loop (up to 10)
+	//CODE:
+	printf("How many times do you want this loop to loop? (up to 10)\n");
+
+	//Scan in the user int selection. Then use the memory address of howMany as the storage for the input.
+	//CODE:
+
+	//Write a for loop that uses a as the counter and initialize it to 1. Then test
+	//a against the maxAmount to see if it is less than or equal to 10. Then increment
+	//i by 2 each iteration.
+	//CODE:
+
+		//print the current value of i on its own line each iteration.
+		//CODE:
+
+		//Check whether i has reached the user-specified limit using a if statement.
+		//set a == howMany. When true, break exits the loop immediately.
+		//CODE:
+	
+}
