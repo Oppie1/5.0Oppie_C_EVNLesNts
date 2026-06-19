d4ed21ce32cd3eda746ad44e94a2a997b4ddc2de
Oppie1 <atindall729@gmail.com>
2026-06-19 13:22:30 -0500

    Add files via upload

---
+++ 1.38CRandmNumGenWthRand.EVLNts/1.38CRandmNumGenWthRand.EVLNts/main.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<math.h>
+
+
+
+
+int main() {
+
+	//Declare an uninitialized int counter variable named i.
+	//CODE:
+
+	//Declare an uninitialized int variable named diceRoll to store that value.
+	//CODE:
+
+	//Create for loop with i initialized to 0, the condition i is less than 10, increments i by 1.
+	//In codeblock Call printf() with digit plus newline and rand() with no parameters
+	//CODE:
+
+	printf("\n\nPrint a Random Dice Roll to the Screen (1-10 total rolls:) \n");
+
+	//Create a for loop that initializes i to 5; a condition i < 10 and increments i by 1
+	//CODE:
+
+		//Assign the variable dice roll to the expression using the rand() function with no parameters modulus % 6
+		//grouped together (expression) and then add 1 to it afterwards.
+		//CODE:
+		
+		//Call a printf() with sentence and new line character that corresponds to the second argument being the variable diceRoll.
+		//CODE:
+	}
---
+++ 1.39CIntNFloatArraysEVNJLNts/1.39CIntNFloatArraysEVNJLNts/main.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+
+
+
+int main() {
+
+	//Declare an uninitialized int var for counter.
+	//CODE:
+
+	//Declare an int array named meatBalls with 5 uninitialized elements.
+	//CODE:
+
+	//Declare an int var called totalMeatBalls and initialize it to "0"
+	//CODE:
+
+	//Use a for loop initialize i to 0, the less than 5 and increment by 1
+	//CODE:
+
+	//Call a printf() to ask user how many meatballs they ate on day i (remember to add 1 since i 0)
+
+	//Call a scanf_s() to scan in the user input for EACH day.
+
+	//Create for loop just like previous one that now scroll though each elements/i value.
+	//CODE:
+
+	//Assign "totalMeatBalls" to expression of each user entry.
+	//CODE:
+
+	//DECLARE an initialized int var named avg that will be assigned a value through total meatballs variable and divides it by
+	//number of days (i)/number of iterations.
+	//CODE:
+
+	//Print to screen a sentence that matches up with  totalMeatBalls as well as avg respectively in each of the two arguments.
+	//CODE:
+
+}
