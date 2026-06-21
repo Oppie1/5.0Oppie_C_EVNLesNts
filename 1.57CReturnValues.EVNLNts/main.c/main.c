a2d29b1845e136a1cd5eaba893dac895162528a5
Oppie1 <atindall729@gmail.com>
2026-06-21 17:57:21 -0500

    Add files via upload

---
+++ 1.57CReturnValues.EVNLNts/main.c/main.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+//Declare a function prototype with int data type named calculateBonus with
+//which takes an int named yearsWorked as its parameter.
+//CODE:
+
+int main() {
+
+	//Declare variable named kanopisBonus and initialize it to the function calculate bonus with 14 as its arg.
+	//CODE:
+
+	//Declare and initialize int variable named petraBonus equal to the function calculate bonus with 3 as its arg.
+	//CODE:
+
+	//Print Kanopi's bonus to the screen.
+	//CODE:
+
+	//Print Petra's bonus to the screen.
+	//CODE:
+
+}
+
+//Define the calculate bonus function,
+//CODE:
+
+
+	//Declare and assign int variable bonus to show what the bonus is for years worked by by 250
+
+	//Use a nested if statement with the condition yearsWorked is greater than 10 they get 10000 more dollars.
+	//CODE:
+
+	//Use a return variable bonus to return the final bonus to the call function in main.
+	//CODE:
---
+++ 1.58CZPassByReference.EVNLNts/1.58CZPassByReference.EVNLNts/main.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+//Declare a void function named passByValue with an uninitialized int parameter i.
+//CODE:
+
+//Declare a void function named passByAddress() with an uninitialized int pointer * parameter name i.
+//CODE:
+
+int main() {
+
+	//Declare an int variable named tuna and initialize it to 20.
+	//CODE:
+
+	//Call the passByValue function with tuna as its parameter.
+	//CODE:
+
+	//Call a printf() that says passing by value did not change the variable in main see: .
+	//CODE:
+
+	//Call the passByAddress() with the MEMORY address of the variable tuna as its parameter
+	//CODE:
+
+	//Call a printf() that says passing by address DID change the variable in main.
+	//CODE:
+
+}
+
+
+//Define the void passByValue function
+//CODE:
+
+//Initialize i to 37.
+//CODE:
+
+//Call a printf function to make sentence saying inside passByValue i is now... with format spec and variable i.
+//CODE:
+
+//Define the void passByAddress function.
+//CODE:
+
+//Initialize pointer i to 64. 
+//CODE:
